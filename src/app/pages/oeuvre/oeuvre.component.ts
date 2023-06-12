41a1980539265cf770d1dba644f9dd4910d81e42
Paul CONVERT <paul.convert@epitech.eu>
2023-06-12 14:53:09 +0200

    test

--- src/app/pages/oeuvre/oeuvre.component.ts
+++ src/app/pages/oeuvre/oeuvre.component.ts
@@ -20,7 +20,7 @@ export class OeuvreComponent {
     
     for (let i = 1; i <= 4; i++) {
       const num = i.toString().padStart(2, '0');
-      const UrlsArchetypes = `${environment.apiUrl}/Archetypes/03/${num}.jpg`;
+      const UrlsArchetypes = ` https://plaumondpicture.s3.eu-west-3.amazonaws.com/Archetypes/03/${num}.jpg`;
       const UrlsDeployments = `${environment.apiUrl}/Deployments/01/${num}.jpg`;
       const UrlsDrawings = `${environment.apiUrl}/Drawings+/01/${num}.jpg`;
       const UrlsTranscriptions = `${environment.apiUrl}/Transcriptions/02/${num}.jpg`;
@@ -35,15 +35,23 @@ export class OeuvreComponent {
 
     this.responsiveOptions = [
       {
-        breakpoint: '1400px',
+        breakpoint: '1440px',
         numVisible: 4,
-        numScroll: 3
+        numScroll: 4
     },
+    
     {
-        breakpoint: '820px',
-        numVisible: 2,
-        numScroll: 2
+      breakpoint: '820px',
+      numVisible: 3,
+      numScroll: 1
     },
+
+    {
+      breakpoint: '620px',
+      numVisible: 2,
+      numScroll: 2
+  },
+  
     {
         breakpoint: '412px',
         numVisible: 1,

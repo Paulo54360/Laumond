e5096304de547b934018166122fc34cb50d5e674
Paul CONVERT <paul.convert@epitech.eu>
2023-06-12 14:12:30 +0200

    add prod route

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    apiBaseUrl: "https://plaumondpicture.s3.eu-west-3.amazonaws.com"
+    apiUrl: "https://plaumondpicture.s3.eu-west-3.amazonaws.com"
   };
   
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: false,
-    apiBaseUrl: "https://plaumondpicture.s3.eu-west-3.amazonaws.com"
+    apiUrl: "/api"
   };
   
